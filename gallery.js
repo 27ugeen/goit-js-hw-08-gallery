29b6f73763a5ef8df7bf859e63b55cc531049684
27ugeen <27ugeen@gmail.com>
2019-10-31 22:27:56 +0200

    create mrkup

---
+++ gallery.js
@@ -0,0 +1,59 @@
+'use strict';
+
+import { default as images } from '../goit-js-hw-08-gallery/gallery-items.js';
+
+const gallery = {
+  listGallery: document.querySelector('.js-gallery')
+};
+
+const addImg = images.filter(({ preview, original, description }) => {
+  gallery.listGallery.insertAdjacentHTML(
+    'afterbegin',
+    `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+  
+      <span class="gallery__icon">
+        <i class="material-icons">zoom_out_map</i>
+      </span>
+    </a>
+  </li>`
+  );
+});
+
+gallery.listGallery.append(addImg);
+
+// function handleClick(e) {
+//   e.preventDefault();
+//   console.log(e.target);
+// }
+
+// gallery.addEventListener('click', handleClick);
+
+// function buildGalleryItem(item) {
+//   return `<li class="gallery__item">
+//     <a
+//       class="gallery__link"
+//       href="${imgGallery.original}"
+//     >
+//       <img
+//         class="gallery__image"
+//         src="${imgGallery.preview}"
+//         data-source="${imgGallery.original}"
+//         alt="${imgGallery.description}"
+//       />
+
+//       <span class="gallery__icon">
+//         <i class="material-icons">zoom_out_map</i>
+//       </span>
+//     </a>
+//   </li>`;
+// }
